3482c435893b90752641e0da19dc76e6f14471fb
Bernard Ojengwa <bernardojengwa@gmail.com>
2015-02-11 16:21:56 +0100

    fixed comma

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,16 +1,14 @@
-import os
-from .base import BASE_DIR
 
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p5t(z#&4dbg*x0$8b74ma!#o^8633t$_qau8cskkv36^fqkf)$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = []
 
