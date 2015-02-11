c7efae3223f2d409a82e6c6eea221bc72a40d67d
Bernard Ojengwa <bernardojengwa@gmail.com>
2015-02-11 16:02:20 +0100

    fixed comma

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -22,7 +22,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'df2aa257fb8cf470aa3263da5d5a01af2',
-        'HOST: '198.11.228.49',
+        'HOST': '198.11.228.49',
         'PORT': '5433',
         'USER': 'ua3d69973664d481d9b700a0fa763bb64',
         'PASSWORD': 'p5d13bda864234463adf757b8c3474371'
