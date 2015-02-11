6a498c761171fc94f013b3e5d296bdf11d73d4fd
Bernard Ojengwa <bernardojengwa@gmail.com>
2015-02-11 16:35:23 +0100

    fixed comma

--- core/settings/test.py
+++ core/settings/test.py
@@ -1,7 +1,9 @@
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))t development settings - unsuitable for production
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
